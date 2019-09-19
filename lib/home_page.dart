a43ffc04ccbe585170c5e4fe1cdefe69b5e9e4ff
xmaihh <xmaihh@protonmail.com>
2019-09-19 17:06:51 +0900

    #LoginPage&HomePage

---
+++ lib/home_page.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 5);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var _tableItems = new List<String>.generate(300, (i) => "第$i行");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TabController'),
+        bottom: TabBar(
+          tabs: <Widget>[
+            Tab(
+              text: '热点',
+            ),
+            Tab(
+              text: '体育',
+            ),
+            Tab(
+              text: '科技',
+            ),
+            Tab(
+              text: '时政',
+            ),
+            Tab(
+              text: '军事',
+            ),
+          ],
+          controller: _tabController, //1
+        ),
+      ),
+      drawer: _drawer,
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          ListView(children: _items),
+          ListView.builder(
+              itemCount: _tableItems.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.book),
+                  title: Text('${_tableItems[index]}'),
+                );
+              }),
+          ListView.separated(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.access_time),
+                  title: Text('${_tableItems[index]}'),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Container(
+                  constraints: BoxConstraints.tightFor(height: 1),
+                  color: Colors.black45,
+                );
+              },
+              itemCount: _tableItems.length),
+          GridView.count(
+            crossAxisCount: 3,
+            children: <Widget>[
+              ListTile(
+                title: Text('item1'),
+              ),
+              ListTile(
+                title: Text('item2'),
+              ),
+              ListTile(
+                title: Text('item3'),
+              ),
+              ListTile(
+                title: Text('item4'),
+              )
+            ],
+          ),
+          PageView(
+            onPageChanged: (index) {
+              print('当前为第 $index 页');
+            },
+            children: <Widget>[
+              ListTile(
+                title: Text('第0页'),
+                subtitle: Text('subtitle'),
+              ),
+              ListTile(
+                title: Text('第1页'),
+                subtitle: Text('subtitle'),
+              ),
+              ListTile(
+                title: Text('第2页'),
+                subtitle: Text('subtitle'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  //实现Drawer示例
+  get _drawer => Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: Icon(Icons.account_circle),
+              accountName: Text('example'),
+              accountEmail: Text('example@gmail.com'),
+            ),
+            ListTile(
+              leading: Icon(Icons.call),
+              title: Text('电话'),
+            ),
+            ListTile(
+              leading: Icon(Icons.local_post_office),
+              title: Text('邮件'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('设置'),
+            ),
+          ],
+        ),
+      );
+
+  //实现ListView Item
+  get _items => <Widget>[
+        ListTile(
+          leading: Icon(Icons.access_time),
+          title: Text('正标题'),
+          subtitle: Text('副标题'),
+        ),
+        ListTile(
+          leading: Icon(Icons.access_time),
+          title: Text('正标题'),
+          subtitle: Text('副标题'),
+        ),
+        ListTile(
+          leading: Icon(Icons.access_time),
+          title: Text('正标题'),
+          subtitle: Text('副标题'),
+        ),
+      ];
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+}
+
+//*TabBar
+class MyTabController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('DefaultTabController示例'),
+            bottom: TabBar(tabs: <Widget>[
+              Tab(
+                text: '热点',
+              ),
+              Tab(
+                text: '体育',
+              ),
+              Tab(
+                text: '科技',
+              ),
+              Tab(
+                text: '教育',
+              ),
+            ]),
+          ),
+          body: TabBarView(children: <Widget>[
+            Center(
+              child: Text('热点'),
+            ),
+            Center(
+              child: Text('体育'),
+            ),
+            Center(
+              child: Text('科技'),
+            ),
+            Center(
+              child: Text('教育'),
+            ),
+          ]),
+        ));
+  }
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,237 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/home_page.dart';
+import 'package:groovin_material_icons/groovin_material_icons.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+  bool _isObscure = true;
+  Color _eyeColor;
+  List _loginMethod = [
+    {
+      "title": "facebook",
+      "icon": GroovinMaterialIcons.facebook,
+    },
+    {
+      "title": "google",
+      "icon": GroovinMaterialIcons.google,
+    },
+    {
+      "title": "twitter",
+      "icon": GroovinMaterialIcons.twitter,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 22.0),
+              children: <Widget>[
+                SizedBox(
+                  height: kToolbarHeight,
+                ),
+                buildTitle(),
+                buildTitleLine(),
+                SizedBox(height: 70.0),
+                buildEmailTextField(),
+                SizedBox(height: 30.0),
+                buildPasswordTextField(context),
+                buildForgetPasswordText(context),
+                SizedBox(height: 60.0),
+                buildLoginButton(context),
+                SizedBox(height: 30.0),
+                buildOtherLoginText(),
+                buildOtherMethod(context),
+                buildRegisterText(context),
+              ],
+            )));
+  }
+
+  Padding buildTitle() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        'Login',
+        style: TextStyle(fontSize: 42.0),
+      ),
+    );
+  }
+
+  Padding buildTitleLine() {
+    return Padding(
+      padding: EdgeInsets.only(left: 12.0, top: 4.0),
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Container(
+          color: Colors.black,
+          width: 40.0,
+          height: 2.0,
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildEmailTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+      ),
+      validator: (String value) {
+        var emailReg = RegExp(
+            r"[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?");
+        if (!emailReg.hasMatch(value)) {
+          return '请输入正确的邮箱地址';
+        }
+      },
+      onSaved: (String value) => _email = value,
+    );
+  }
+
+  TextFormField buildPasswordTextField(BuildContext context) {
+    return TextFormField(
+      onSaved: (String value) => _password = value,
+      obscureText: _isObscure,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return '请输入密码';
+        }
+      },
+      decoration: InputDecoration(
+        labelText: 'Password',
+        suffixIcon: IconButton(
+          icon: Icon(
+            Icons.remove_red_eye,
+            color: _eyeColor,
+          ),
+          onPressed: () {
+            setState(() {
+              _isObscure = !_isObscure;
+              _eyeColor =
+                  _isObscure ? Colors.grey : Theme.of(context).iconTheme.color;
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  Padding buildForgetPasswordText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: FlatButton(
+          child: Text(
+            '忘记密码?',
+            style: TextStyle(fontSize: 14.0, color: Colors.grey),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+
+  Align buildLoginButton(BuildContext context) {
+    return Align(
+      child: SizedBox(
+        height: 45.0,
+        width: 270.0,
+        child: RaisedButton(
+          child: Text(
+            'Login',
+            style: Theme.of(context).primaryTextTheme.headline,
+          ),
+          color: Colors.black,
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              /// 只有输入内容符合通过要求才能到达此处
+              _formKey.currentState.save();
+
+              /// TODO 执行登录方法
+              print('email:$_email, password:$_password');
+
+              Navigator.push(context,
+                  new MaterialPageRoute(builder: (context) => HomePage()));
+            }
+          },
+          shape: StadiumBorder(side: BorderSide()),
+        ),
+      ),
+    );
+  }
+
+  Align buildOtherLoginText() {
+    return Align(
+      alignment: Alignment.center,
+      child: Text(
+        '其他帐号登录',
+        style: TextStyle(color: Colors.grey, fontSize: 14.0),
+      ),
+    );
+  }
+
+  ButtonBar buildOtherMethod(BuildContext context) {
+    return ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: _loginMethod
+          .map((item) => Builder(
+                builder: (context) {
+                  return IconButton(
+                      icon: Icon(
+                        item['icon'],
+                        color: Theme.of(context).iconTheme.color,
+                      ),
+                      onPressed: () {
+                        /// TODO 第三方登录方法
+                        Scaffold.of(context).showSnackBar(new SnackBar(
+                          content: new Text("${item['title']}登录"),
+                          action: new SnackBarAction(
+                            label: '取消',
+                            onPressed: () {},
+                          ),
+                        ));
+                      });
+                },
+              ))
+          .toList(),
+    );
+  }
+
+  Align buildRegisterText(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: EdgeInsets.only(top: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('没有帐号?'),
+            GestureDetector(
+              child: Text(
+                '点击注册',
+                style: TextStyle(color: Colors.green),
+              ),
+              onTap: () {
+                /// TODO 跳转到注册页面
+                print('去注册');
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
