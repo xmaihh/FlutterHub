7c8c37445f776c2d2b4af07f01c8a0f2f564d578
xmaihh <xmaihh@protonmail.com>
2024-07-01 11:48:41 +0800

    feat: :sparkles: "Refactor TapboxB and TapboxC to use GestureDetector for handling tap events."

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_compass/widgets/sunrise_painter.dart';
 
 import 'widgets/dial_page.dart';
 
@@ -160,7 +159,7 @@ class _TapboxAState extends State<TapboxA> {
         child: Center(
           child: Text(
             _active ? 'Active' : 'Inactive',
-          style: const TextStyle(fontSize: 32.0, color: Colors.white),
+            style: const TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
       ),
@@ -175,6 +174,7 @@ class ParentWidget extends StatefulWidget {
   @override
   State<ParentWidget> createState() => _ParentWidgetState();
 }
+
 class _ParentWidgetState extends State<ParentWidget> {
   bool _active = false;
 
@@ -194,10 +194,10 @@ class _ParentWidgetState extends State<ParentWidget> {
         ),
       ),
     );
- }
- }
+  }
+}
 
- // TapboxB
+// TapboxB
 class TapboxB extends StatelessWidget {
   const TapboxB({super.key, this.active = false, required this.onChanged});
 
@@ -211,21 +211,80 @@ class TapboxB extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: handleTap,
+        onTap: handleTap,
+        child: Container(
+          width: 200.0,
+          height: 200.0,
+          decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[600],
+          ),
+          child: Center(
+            child: Text(
+              active ? 'Active' : 'Inactive',
+              style: const TextStyle(fontSize: 32.0, color: Colors.white),
+            ),
+          ),
+        ));
+  }
+}
+
+class TapboxC extends StatefulWidget {
+  const TapboxC({super.key, this.active = false, required this.onChanged});
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  State<TapboxC> createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
       child: Container(
         width: 200.0,
         height: 200.0,
         decoration: BoxDecoration(
-          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(color: Colors.teal[700]!, width: 10.0)
+              : null,
         ),
         child: Center(
           child: Text(
-            active ? 'Active' : 'Inactive',
+            widget.active ? 'Active' : 'Inactive',
             style: const TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
-      )
+      ),
     );
   }
 }
-
