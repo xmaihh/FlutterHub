44247f12f888f13b8e93cedaaa643f0c69a91267
xmaihh <xmaihh@protonmail.com>
2024-07-18 09:11:42 +0800

    style: :lipstick: remove unused widgets and background image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_compass/route/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,9 +37,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      // home: const DialPage(),
-      // home: const PieChartSample2(),
-      // home: const SunriseChart(),
     );
   }
 }
@@ -120,8 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const RandomWordsWidget(),
+            )
           ],
         ),
       ),
@@ -130,167 +125,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-// TapboxA 管理自身状态
-class TapboxA extends StatefulWidget {
-  const TapboxA({super.key});
-
-  @override
-  State<TapboxA> createState() => _TapboxAState();
-}
-
-class _TapboxAState extends State<TapboxA> {
-  bool _active = false;
-
-  void _handleTap() {
-    setState(() {
-      _active = !_active;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Container(
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
-        child: Center(
-          child: Text(
-            _active ? 'Active' : 'Inactive',
-            style: const TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// ParentWidget 为TapboxB 管理状态
-class ParentWidget extends StatefulWidget {
-  const ParentWidget({super.key});
-
-  @override
-  State<ParentWidget> createState() => _ParentWidgetState();
-}
-
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: TapboxB(
-          active: _active,
-          onChanged: _handleTapboxChanged,
-        ),
-      ),
-    );
-  }
-}
-
-// TapboxB
-class TapboxB extends StatelessWidget {
-  const TapboxB({super.key, this.active = false, required this.onChanged});
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  void handleTap() {
-    onChanged(!active);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: handleTap,
-        child: Container(
-          width: 200.0,
-          height: 200.0,
-          decoration: BoxDecoration(
-            color: active ? Colors.lightGreen[700] : Colors.grey[600],
-          ),
-          child: Center(
-            child: Text(
-              active ? 'Active' : 'Inactive',
-              style: const TextStyle(fontSize: 32.0, color: Colors.white),
-            ),
-          ),
-        ));
-  }
-}
-
-class TapboxC extends StatefulWidget {
-  const TapboxC({super.key, this.active = false, required this.onChanged});
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  @override
-  State<TapboxC> createState() => _TapboxCState();
-}
-
-class _TapboxCState extends State<TapboxC> {
-  bool _highlight = false;
-
-  void _handleTapDown(TapDownDetails details) {
-    setState(() {
-      _highlight = true;
-    });
-  }
-
-  void _handleTapUp(TapUpDetails details) {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTapCancel() {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTap() {
-    widget.onChanged(!widget.active);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: _handleTapDown,
-      onTapUp: _handleTapUp,
-      onTap: _handleTap,
-      onTapCancel: _handleTapCancel,
-      child: Container(
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
-          border: _highlight
-              ? Border.all(color: Colors.teal[700]!, width: 10.0)
-              : null,
-        ),
-        child: Center(
-          child: Text(
-            widget.active ? 'Active' : 'Inactive',
-            style: const TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-      ),
     );
   }
 }
