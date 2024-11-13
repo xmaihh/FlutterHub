dea79d22bd69eebb9bc4e1f12b4a2d47dd802537
xmaihh <xmaihh@protonmail.com>
2024-11-13 17:12:54 +0800

    style: :lipstick: refactor article detail page for improved readability and structure

--- lib/routes/home/article_detail_page.dart
+++ lib/routes/home/article_detail_page.dart
@@ -6,7 +6,8 @@ class ArticleDetailPage extends StatefulWidget {
   final String url;
   final String title;
   final bool collect;
-  ArticleDetailPage({required this.url, required this.title,this.collect = false});
+
+  ArticleDetailPage({required this.url, required this.title, this.collect = false});
 
   @override
   State<ArticleDetailPage> createState() => _ArticleDetailPageState();
@@ -29,10 +30,12 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
               webViewController?.reload();
             },
           ),
-          IconButton(onPressed: () {}, icon: Icon(
-            widget.collect ? Icons.favorite : Icons.favorite_border,
-            color: widget.collect ? Colors.red : Colors.grey,
-          )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                widget.collect ? Icons.favorite : Icons.favorite_border,
+                color: widget.collect ? Colors.red : Colors.grey,
+              )),
           IconButton(
               onPressed: () {
                 launchUrl(Uri.parse(widget.url), mode: LaunchMode.externalApplication);
@@ -57,6 +60,9 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
                 webViewController = controller;
               },
               shouldOverrideUrlLoading: _shouldOverrideUrlLoading,
+              onConsoleMessage: (controller, consoleMessage) {
+                print("CONSOLE MESSAGE: ${consoleMessage.message}");
+              },
               onLoadStart: (controller, url) {
                 setState(() {
                   progress = 0;
@@ -66,6 +72,10 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
                 setState(() {
                   this.progress = progress / 100;
                 });
+              },
+              onPermissionRequest: (controller, request) async {
+                print(request);
+                return PermissionResponse(resources: request.resources, action: PermissionResponseAction.GRANT);
               },
               onLoadStop: (controller, url) async {
                 setState(() {
@@ -79,38 +89,30 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
     );
   }
 
-  Future<NavigationActionPolicy> _shouldOverrideUrlLoading(
-      InAppWebViewController controller, NavigationAction navigationAction) async {
+  Future<NavigationActionPolicy> _shouldOverrideUrlLoading(InAppWebViewController controller, NavigationAction navigationAction) async {
     var uri = navigationAction.request.url!;
 
-    if (![
-      "http",
-      "https",
-      "file",
-      "chrome",
-      "data",
-      "javascript",
-      "about"
-    ].contains(uri.scheme)) {
+    if (!["http", "https", "file", "chrome", "data", "javascript", "about"].contains(uri.scheme)) {
       if (await canLaunchUrl(uri)) {
         // 对于非 HTTP(S) URL，询问用户是否在外部打开
         bool shouldLaunch = await showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: Text("外部链接"),
-            content: Text("是否打开外部应用 ${uri.scheme}?"),
-            actions: <Widget>[
-              TextButton(
-                child: Text("取消"),
-                onPressed: () => Navigator.of(context).pop(false),
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text("外部链接"),
+                content: Text("是否打开外部应用 ${uri.scheme}?"),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text("取消"),
+                    onPressed: () => Navigator.of(context).pop(false),
+                  ),
+                  TextButton(
+                    child: Text("打开"),
+                    onPressed: () => Navigator.of(context).pop(true),
+                  ),
+                ],
               ),
-              TextButton(
-                child: Text("打开"),
-                onPressed: () => Navigator.of(context).pop(true),
-              ),
-            ],
-          ),
-        ) ?? false;
+            ) ??
+            false;
 
         if (shouldLaunch) {
           await launchUrl(uri);
@@ -122,22 +124,23 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
     // 检查是否是恶意跳转（例如淘宝）
     if (uri.host.contains('taobao.com') || uri.host.contains('tmall.com')) {
       bool shouldLoad = await showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text("警告"),
-          content: Text("检测到跳转到电商网站，是否继续？"),
-          actions: <Widget>[
-            TextButton(
-              child: Text("取消"),
-              onPressed: () => Navigator.of(context).pop(false),
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text("警告"),
+              content: Text("检测到跳转到电商网站，是否继续？"),
+              actions: <Widget>[
+                TextButton(
+                  child: Text("取消"),
+                  onPressed: () => Navigator.of(context).pop(false),
+                ),
+                TextButton(
+                  child: Text("继续"),
+                  onPressed: () => Navigator.of(context).pop(true),
+                ),
+              ],
             ),
-            TextButton(
-              child: Text("继续"),
-              onPressed: () => Navigator.of(context).pop(true),
-            ),
-          ],
-        ),
-      ) ?? false;
+          ) ??
+          false;
 
       if (!shouldLoad) {
         return NavigationActionPolicy.CANCEL;
